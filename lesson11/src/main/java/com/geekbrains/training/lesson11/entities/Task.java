2e89a7d7a9edd29e87a2d35da301b9a59e3a646b
yura7711 <yura7711@gmail.com>
2019-11-22 00:41:11 +0500

    Урок 11. Hibernate

---
+++ lesson11/src/main/java/com/geekbrains/training/lesson11/entities/Task.java
@@ -0,0 +1,142 @@
+package com.geekbrains.training.lesson11.entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="tasks")
+public class Task {
+    private static final long serialVersionUID = -277821547227621214L;
+
+    public enum Status {
+        created(1,"Создана", 1)
+        , inWork(2,"В работе", 2)
+        , closed(3,"Закрыта", 3)
+        , rejected(4,"Отклонена", 4);
+
+        private int statusId;
+        private String rusTitle;
+        private int priority;
+
+        Status(int statusId, String rusTitle, int priority) {
+            this.statusId = statusId;
+            this.rusTitle = rusTitle;
+            this.priority = priority;
+        }
+
+        public String getRusTitle() {
+            return rusTitle;
+        }
+
+        public int getPriority() {
+            return priority;
+        }
+
+        public int getStatusId() { return statusId; }
+
+        public static Status getStatusByRusName(String rusName){
+            for (Status status: Status.values()) {
+                if (status.getRusTitle().equals(rusName)){
+                    return status;
+                }
+            }
+            return null;
+        }
+
+        public static Status getStatusById(int statusId){
+            for (Status status: Status.values()) {
+                if (status.getStatusId() == statusId){
+                    return status;
+                }
+            }
+            return null;
+        }
+    }
+
+    @Id
+    @Column(name="task_id")
+    private Long id;
+
+    @Column(name="task_name")
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name="author_id")
+    private User author;
+
+    @ManyToOne
+    @JoinColumn(name="executor_id")
+    private User executor;
+
+    @Column(name="description")
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    public Task(Long id, String name, User author, User executor, String description) {
+        this.id = id;
+        this.name = name;
+        this.author = author;
+        this.executor = executor;
+        this.description = description;
+        this.status = Status.created;
+    }
+
+    public Task(Long id, String name, User author, User executor, String description, Status status) {
+        this.id = id;
+        this.name = name;
+        this.author = author;
+        this.executor = executor;
+        this.description = description;
+        this.status = status;
+    }
+
+    public Task() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public User getExecutor() {
+        return executor;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getStatus() {
+        return status.getRusTitle();
+    }
+
+    public Status getEnumStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof Task)){
+            return false;
+        }
+        return this.id.equals(((Task)obj).getId());
+
+    }
+
+    @Override
+    public String toString() {
+        return "***\nid=" + id + " " + name + "\nСтатус: " + status.getRusTitle() + "\nИсполнитель: " + executor.getUserName() + "\nАвтор: " + author.getUserName() + "\nОписание: " + description;
+    }
+}
