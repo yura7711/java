2b97647b77162b41956e02bce7453ebf0ef75e57
yura7711 <yura7711@gmail.com>
2019-11-24 19:22:19 +0500

    Урок 12. Spring Core

--- lesson12/src/main/java/com/geekbrains/training/lesson11/repositories/TaskRepository.java
+++ lesson12/src/main/java/com/geekbrains/training/lesson11/repositories/TaskRepository.java
@@ -5,12 +5,14 @@ import com.geekbrains.training.lesson11.entities.Task;
 import java.util.List;
 
 public interface TaskRepository {
-    void prepare();
     void shutdown();
+    boolean checkTaskById(Long id);
     void addTask(Long id, String name, Long author_id, Long executor_id, String description);
     List<Task> getTaskArray();
     void updateTaskStatus(Long idTask, Task.Status newStatus);
     void deleteTask(Long idTask);
     void deleteTask(String nameTask);
     List<Task> getTasksForMe(Long userId);
+    int getCountTaskByStatus(Task.Status status);
+    List<Task> getTaskByStatus(Task.Status status);
 }
