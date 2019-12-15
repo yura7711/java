48849f70dac39f8c866de3179b8e8ac4186f3c94
yura7711 <yura7711@gmail.com>
2019-12-15 18:28:15 +0500

    Урок 17. GWT

---
+++ lesson17v3/spring-gwt/spring/src/main/java/com/geekbrains/server/controllers/TaskController.java
@@ -0,0 +1,67 @@
+package com.geekbrains.server.controllers;
+
+import com.geekbrains.gwt.common.TaskAddDto;
+import com.geekbrains.gwt.common.TaskDto;
+import com.geekbrains.server.entities.Task;
+import com.geekbrains.server.entities.User;
+import com.geekbrains.server.services.TaskService;
+import com.geekbrains.server.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@CrossOrigin
+public class TaskController {
+    TaskService myTaskService;
+    UserService myUserService;
+
+    @Autowired
+    public void setTaskService(TaskService taskService) {
+        this.myTaskService = taskService;
+    }
+
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.myUserService = userService;
+    }
+
+    @GetMapping("/tasks")
+    public List<TaskDto> getAllItems(@RequestParam(value = "statusId", required = false) Integer statusId
+            , @RequestParam(value = "executer_id", required = false) Long executer_id
+            , @RequestParam(value = "author_id", required = false) Long author_id
+    ) {
+        List<TaskDto> tasks = myTaskService.findAllTaskDto(executer_id, author_id, statusId);
+        return tasks;
+    }
+
+    @GetMapping("/tasks/statuses")
+    public List<Task.Status> getStatuses() {
+        List<Task.Status> statuses = new ArrayList<>();
+        for (Task.Status o: Task.Status.values()) {
+            statuses.add(o);
+        }
+        return statuses;
+    }
+
+    @GetMapping("/tasks/users")
+    public List<User> getUsers() {
+        return myUserService.getAllUsers();
+    }
+
+    @DeleteMapping("tasks/remove/{id}")
+    public ResponseEntity<String> removeTask(@PathVariable Long id) {
+        myTaskService.deleteTask(id);
+        return new ResponseEntity<String>("Successfully removed", HttpStatus.OK);
+    }
+
+    @PostMapping("tasks/add")
+    public ResponseEntity<String> createTask(@ModelAttribute TaskAddDto taskAddDto) {
+        myTaskService.addTask(taskAddDto);
+        return new ResponseEntity<String>("Successfully created", HttpStatus.OK);
+    }
+}
